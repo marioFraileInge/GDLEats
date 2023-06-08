6e18cf49eb48264a596b68edaab1097175660dc0
Mario Fraile <mariofraileing@gmail.com>
2023-06-08 11:48:24 -0600

    Initial Commit

---
+++ GDLEats/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GDLEats
+//
+//  Created by Mario AFH on 08/06/23.
+//  Copyright © 2023 MAFHDev. All rights reserved.
+//  Using Swift 5.0
+//
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GDLEatsUITests/GDLEatsUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  GDLEatsUITestsLaunchTests.swift
+//  GDLEatsUITests
+//
+//  Created by Mario AFH on 08/06/23.
+//  Copyright © 2023 MAFHDev. All rights reserved.
+//  Using Swift 5.0
+//
+        
+
+import XCTest
+
+final class GDLEatsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
